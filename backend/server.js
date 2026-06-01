d91aea0802fb75205641951e2a27508fb775b553
Ayush shah <shahayush091@gmail.com>
2026-06-01 12:44:09 +0530

    fix connection issue in server file

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
 
-  console.log("User connected:", socket.id);
+  // console.log("User connected:", socket.id);
 
   socket.on("join_channel", (channelId) => {
       socket.join(channelId);
@@ -70,9 +70,9 @@ io.on("connection", (socket) => {
 
     }
 });
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
+  // socket.on("disconnect", () => {
+  //   console.log("User disconnected:", socket.id);
+  // });
 
 });
 
